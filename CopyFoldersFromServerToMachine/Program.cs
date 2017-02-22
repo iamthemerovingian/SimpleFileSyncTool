ffb18f59457b6a8d97c04deb4d2977c51aa01808
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-02-22 16:32:22 +0700

    So far I am unable to get the target location programmically. Looking for solutions.

--- CopyFoldersFromServerToMachine/Program.cs
+++ CopyFoldersFromServerToMachine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,81 @@ namespace CopyFoldersFromServerToMachine
     {
         static void Main(string[] args)
         {
+            string SystemReflectionAssemblyGetExecutingAssemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            Console.WriteLine("SystemReflectionAssemblyGetExecutingAssemblyLocation: \n" + SystemReflectionAssemblyGetExecutingAssemblyLocation);
+            Console.WriteLine();
+
+            string SystemReflectionAssemblyGetExecutingAssemblyCodeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            Console.WriteLine("SystemReflectionAssemblyGetExecutingAssemblyCodeBase: \n" + SystemReflectionAssemblyGetExecutingAssemblyCodeBase);
+            Console.WriteLine();
+
+            string PathGetDirectoryNameSystemReflectionAssemblyGetExecutingAssemblyLocation = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            Console.WriteLine("PathGetDirectoryNameSystemReflectionAssemblyGetExecutingAssemblyLocation: \n" + PathGetDirectoryNameSystemReflectionAssemblyGetExecutingAssemblyLocation);
+            Console.WriteLine();
+
+            string AppdomainCurrentDomainBaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine("AppdomainCurrentDomainBaseDirectory: \n" + AppdomainCurrentDomainBaseDirectory);
+            Console.WriteLine();
+
+            string DirectoryGetCurrentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine("DirectoryGetCurrentDirectory: \n" + DirectoryGetCurrentDirectory);
+            Console.WriteLine();
+
+            string EnviromentCurrentDirectory = Environment.CurrentDirectory;
+            Console.WriteLine("DirectoryGetCurrentDirectory: \n" + EnviromentCurrentDirectory);
+            Console.WriteLine();
+
+            string PathGetDirectoryNameSystemReflectionAssemblyGetExecutingAssemblyGetNameCodeBase = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            Console.WriteLine("PathGetDirectoryNameSystemReflectionAssemblyGetExecutingAssemblyGetNameCodeBase: \n" + PathGetDirectoryNameSystemReflectionAssemblyGetExecutingAssemblyGetNameCodeBase);
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+            //string fileName = "TestDataToCopy.txt";
+            //string sourceFolder = @"\\lp7aps15.calcomp.co.th\public\Milinda\CopyDatatest";
+            //string sourceLocation = AppDomain.CurrentDomain.BaseDirectory;
+            //string sourcePath = sourceLocation + sourceFolder;
+            //string targetPath = @"C:\TargetFolder";
+
+            //DirectoryCopy(sourcePath, targetPath, true);
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
         }
     }
 }
--- CopyFoldersFromServerToMachine/Properties/AssemblyInfo.cs
+++ CopyFoldersFromServerToMachine/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CopyFoldersFromServerToMachine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ServerSynApplication")]
+[assembly: AssemblyDescription("This app is designed to sync folders in the server to pcs in the production line.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CopyFoldersFromServerToMachine")]
+[assembly: AssemblyProduct("SyncWithServer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
